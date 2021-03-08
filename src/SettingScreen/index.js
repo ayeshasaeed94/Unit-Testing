fd1edfee484696159497b5bf2fc72605f92b8743
Ayesha <ayeshasaeed713@gmail.com>
2021-03-08 19:33:23 +0500

    State added in settings component

--- src/SettingScreen/index.js
+++ src/SettingScreen/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -9,11 +10,14 @@ import {
 
 const SettingsScreen = (props) => {
 
+    const [count, setCount] = useState(0);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.container}>
-        <Text>This is Settings Screen</Text>
+        <Text>{`You clicked ${count} times`}</Text>
+        <Button title="Click me" onPress={() => setCount(count + 1)}/>
         <Button title="Go Back" onPress={() => props.navigation.pop()}/>
       </SafeAreaView>
     </>
